1cf0011e30d29fcea3df36f6283f9bb7757c8795
RyoTakei <takeiryou926@gmail.com>
2018-06-17 15:59:44 -0700

    4.1.2 prob8 done

--- core/src/org/pltw/examples/game/WorldModel.java
+++ core/src/org/pltw/examples/game/WorldModel.java
@@ -15,31 +15,28 @@ public class WorldModel {
     private Sprite tempSprite;
 
     public WorldModel() {
-        initSampleMap();
+        initSampleMap1();
     }
 
     private void initSampleMap() {
         for (int i = 0; i < Constants.HEIGHT; i++) { //for each row in the map
-            for (int j = 0; j < Constants.WIDTH; j++){ // for each column in the map
+            for (int j = 0; j < Constants.WIDTH; j++) { // for each column in the map
                 if (i != j || i != 0) {
-                    tempSpriteX = 0-i*Constants.X_PADDING_TO_NEXT_TILE +
-                            j*Constants.X_PADDING_TO_NEXT_TILE;
+                    tempSpriteX = 0 - i * Constants.X_PADDING_TO_NEXT_TILE +
+                            j * Constants.X_PADDING_TO_NEXT_TILE;
                     // Case: i or j indicate a wall sprite (value of 0)
                     if (i == Constants.WALL_ROW || j == Constants.WALL_COL) {
                         setupWallSprite(i, j);
                     }
                     // Case: i and j are both non-0 indicates a floor tile sprite
                     else {
-                        /* ToDo add conditionals, methods, and method calls like the one below
-                        per the instructions in Part IV of Activity 4.1.1 */
-                        initSampleMap1(i, j);
 
                         tempSprite.setSize(Constants.TILE_WIDTH, Constants.TILE_HEIGHT);
-                        tempSpriteY = -0.0f-i*Constants.Y_PADDING_TO_NEXT_TILE -
-                                j*Constants.Y_PADDING_TO_NEXT_TILE;
+                        tempSpriteY = -0.0f - i * Constants.Y_PADDING_TO_NEXT_TILE -
+                                j * Constants.Y_PADDING_TO_NEXT_TILE;
                     }
 
-                    tempSprite.setPosition(tempSpriteX,tempSpriteY);
+                    tempSprite.setPosition(tempSpriteX, tempSpriteY);
                     sampleMap[i][j] = tempSprite;
                 }
             }
@@ -54,25 +51,24 @@ public class WorldModel {
         tempSprite = new Sprite(Assets.instance.roomTiles.tileBlack);
     }
 
-    private void woodFloorTile(){
+    private void woodFloorTile() {
         tempSprite = new Sprite(Assets.instance.roomTiles.tileWood);
     }
 
-    private void setupWallSprite(int i, int j){
-            tempSprite = new Sprite(Assets.instance.roomTiles.wall1Blank);
+    private void setupWallSprite(int i, int j) {
+        tempSprite = new Sprite(Assets.instance.roomTiles.wall1Blank);
 
-            // Right side wall sprites must be flipped
-            if (i == Constants.WALL_ROW) {
-                tempSprite.flip(true, false); // flip along the Y axis
-                tempSpriteY = - j*Constants.Y_PADDING_TO_NEXT_TILE;
-            }
-            else {
-                tempSpriteX = tempSpriteX + Constants.WALL_TILE_WIDTH;
-                tempSpriteY = -i*Constants.Y_PADDING_TO_NEXT_TILE +
-                        j*Constants.Y_PADDING_TO_NEXT_TILE;
-            }
+        // Right side wall sprites must be flipped
+        if (i == Constants.WALL_ROW) {
+            tempSprite.flip(true, false); // flip along the Y axis
+            tempSpriteY = -j * Constants.Y_PADDING_TO_NEXT_TILE;
+        } else {
+            tempSpriteX = tempSpriteX + Constants.WALL_TILE_WIDTH;
+            tempSpriteY = -i * Constants.Y_PADDING_TO_NEXT_TILE +
+                    j * Constants.Y_PADDING_TO_NEXT_TILE;
+        }
 
-            tempSprite.setSize(Constants.WALL_TILE_WIDTH, Constants.WALL_TILE_HEIGHT);
+        tempSprite.setSize(Constants.WALL_TILE_WIDTH, Constants.WALL_TILE_HEIGHT);
     }
 
     private void oddRowStripeFloor(int i, int j) {
@@ -82,6 +78,7 @@ public class WorldModel {
             tempSprite = new Sprite(Assets.instance.roomTiles.tileBlue);
         }
     }
+
     private void oddColStripeFloor(int i, int j) {
         if (j % 2 != 0) {
             tempSprite = new Sprite(Assets.instance.roomTiles.tileBlack);
@@ -108,12 +105,23 @@ public class WorldModel {
         }
     }
 
-    private void initSampleMap1(int i, int j) {
+    private void initSampleMap1() {
         int[][] map = LevelLoader.load();
-        if (map[i][j] == 1) {
-            woodFloorTile();
-        } else {
-            blueFloorTile();
+        for (int i = 0; i < Constants.HEIGHT; i++) { //for each row in the map
+            for (int j = 0; j < Constants.WIDTH; j++) { // for each column in the map
+                tempSpriteX = 0 - i * Constants.X_PADDING_TO_NEXT_TILE +
+                        j * Constants.X_PADDING_TO_NEXT_TILE;
+                if (map[i][j] == 1) {
+                    woodFloorTile();
+                } else {
+                    blueFloorTile();
+                }
+                tempSprite.setSize(Constants.TILE_WIDTH, Constants.TILE_HEIGHT);
+                tempSpriteY = -0.0f - i * Constants.Y_PADDING_TO_NEXT_TILE -
+                        j * Constants.Y_PADDING_TO_NEXT_TILE;
+                tempSprite.setPosition(tempSpriteX, tempSpriteY);
+                sampleMap[i][j] = tempSprite;
+            }
         }
     }
 }
